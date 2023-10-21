2076e20edf1c5699d6390beb1fdf0a07e55bb7a1
Alainfuren <94890779+Alainfuren@users.noreply.github.com>
2023-10-22 00:41:03 +0800

    Update predict_behavior_fit.m

--- cpm_script/predict_behavior_fit.m
+++ cpm_script/predict_behavior_fit.m
@@ -1,84 +1,82 @@
-function [true_predict_r_pos, true_predict_r_neg, consensus_feature] = predict_behavior_fit(all_mats,all_behav,thresh)
-warning off;
+function [true_predict_r_pos, true_predict_r_neg, consensus_feature] = predict_behavior_fit(all_mats, all_behav, thresh)
+    warning off;
 
-function [true_predict_r_pos, true_predict_r_neg, consensus_feature] = predict_behavior_fit(all_mats,all_behav,thresh)
-warning off;
+    % Get the number of subjects and the number of nodes
+    no_sub = size(all_mats, 3);
+    no_node = size(all_mats, 1);
 
-%求得被试数量、节点数量
-no_sub = size(all_mats,3);
-no_node = size(all_mats,1);
+    % Declare empty arrays (initialized with 0) to store model predictions on the test set and edges significantly present in all samples except one
+    behav_pred_pos = zeros(no_sub, 1);
+    behav_pred_neg = zeros(no_sub, 1);
+    consensus_feature = zeros(no_node, no_node, no_sub);
 
-%声明空数组（值为0），后续用来存储模型在测试集上的预测结果、以及除留一样本外所有样本内都显著的连边
-behav_pred_pos = zeros(no_sub,1);
-behav_pred_neg = zeros(no_sub,1);
-consensus_feature = zeros(no_node,no_node,no_sub);
+    % Estimate model accuracy using leave-one-out cross-validation
+    for leftout = 1:no_sub
 
-%利用留一交叉验证法估计模型精度
-for leftout = 1:no_sub
-    
-    %打印出留一的样本
-    fprintf('\n Leaving out subj # %f', leftout);
-    %得到训练集并将留一的样本剔除
-    train_mats = all_mats;
-    train_mats(:,:,leftout) = []; 
-    %重排训练集，将FC矩阵展开成一行特征向量；对于实例数据，最终尺寸为8100*77
-    train_vcts = reshape(train_mats,[],size(train_mats,3));  
-    %得到量表得分（即“标签”），并将留一样本的得分删除；对于实例数据，最终尺寸为77*1
-    train_behav = all_behav; 
-    train_behav(leftout) = [];
-    
-    %计算每条连边与量表得分的相关值
-    [r_mat,p_mat] = corr(train_vcts',train_behav);
-    %重排计算相关得到的r值和p值；对于实例数据，重排为90*90
-    r_mat = reshape(r_mat,no_node,no_node);
-    p_mat = reshape(p_mat,no_node,no_node);
-    
-    %声明两个空数组，后续用来表示显著的正连接和负连接
-    pos_mask = zeros(no_node,no_node);
-    neg_mask = zeros(no_node,no_node);
-    
-    %找到FC>0且与量表显著正相关的连边
-    pos_edges = find(r_mat > 0 & p_mat < thresh); 
-    %找到FC<0且与量表显著负相关的连边
-    neg_edges = find(r_mat < 0 & p_mat < thresh);  
-    %二值化，与量表显著相关的连边置1，其他置0,得到正、负连边的mask
-    pos_mask(pos_edges) = 1;
-    neg_mask(neg_edges) = 1;
-    
-    %对于本次fold，得到的正、负显著连边mask的并集
-    consensus_feature(:,:,leftout) = 1*pos_mask + (-1)*neg_mask;
-    
-    %声明两个空数组，后续用来存储正、负显著连边FC的和
-    train_sumpos = zeros(no_sub-1,1);
-    train_sumneg = zeros(no_sub-1,1);
-    %对于留一之外的其他样本，求得每一个样本的正、负显著连边FC的和，除以2是因为矩阵是对称的
-    for ss=1:size(train_sumpos)
-        train_sumpos(ss) = sum(sum(train_mats(:,:,ss).*pos_mask))/2;
-        train_sumneg(ss) = sum(sum(train_mats(:,:,ss).*neg_mask))/2;
+        % Print the left-out subject
+        fprintf('\n Leaving out subj # %f', leftout);
+
+        % Obtain the training set and remove the left-out subject
+        train_mats = all_mats;
+        train_mats(:, :, leftout) = [];
+
+        % Reshape the training set, flattening FC matrices into feature vectors; for the example data, the final size is 8100x77
+        train_vcts = reshape(train_mats, [], size(train_mats, 3));
+
+        % Get the behavioral scores ("labels") and remove the score of the left-out subject; for the example data, the final size is 77x1
+        train_behav = all_behav;
+        train_behav(leftout) = [];
+
+        % Calculate the correlation of each edge with the behavioral score
+        [r_mat, p_mat] = corr(train_vcts', train_behav);
+
+        % Reshape the correlation results (r values and p values); for the example data, reshape to 90x90
+        r_mat = reshape(r_mat, no_node, no_node);
+        p_mat = reshape(p_mat, no_node, no_node);
+
+        % Declare two empty arrays to represent significant positive and negative connections
+        pos_mask = zeros(no_node, no_node);
+        neg_mask = zeros(no_node, no_node);
+
+        % Find edges with FC>0 and significantly positively correlated with the behavioral score
+        pos_edges = find(r_mat > 0 & p_mat < thresh);
+
+        % Find edges with FC<0 and significantly negatively correlated with the behavioral score
+        neg_edges = find(r_mat < 0 & p_mat < thresh);
+
+        % Binarize, setting edges significantly correlated with the behavioral score to 1 and others to 0, to obtain positive and negative edge masks
+        pos_mask(pos_edges) = 1;
+        neg_mask(neg_edges) = 1;
+
+        % For this fold, obtain the sums of positive and negative significant edge FC
+        train_sumpos = zeros(no_sub - 1, 1);
+        train_sumneg = zeros(no_sub - 1, 1);
+
+        % For all subjects except the left-out one, calculate the sums of positive and negative significant edge FC and divide by 2 since the matrix is symmetric
+        for ss = 1:size(train_sumpos)
+            train_sumpos(ss) = sum(sum(train_mats(:, :, ss) .* pos_mask)) / 2;
+            train_sumneg(ss) = sum(sum(train_mats(:, :, ss) .* neg_mask)) / 2;
+        end
+
+        % Use polyfit function (linear model) to fit the training features and behavioral scores, obtaining coefficients
+        % In this example, we use the sums of positive and negative significant edge FC for fitting
+        fit_pos = polyfit(train_sumpos, train_behav, 1);
+        fit_neg = polyfit(train_sumneg, train_behav, 1);
+
+        % Retrieve the test sample left out, and calculate the sums of positive and negative significant edges
+        test_mat = all_mats(:, :, leftout);
+        test_sumpos = sum(sum(test_mat .* pos_mask)) / 2;
+        test_sumneg = sum(sum(test_mat .* neg_mask)) / 2;
+
+        % Use the fitted linear model to predict the behavioral score
+        behav_pred_pos(leftout) = fit_pos(1) * test_sumpos + fit_pos(2);
+        behav_pred_neg(leftout) = fit_neg(1) * test_sumneg + fit_neg(2);
     end
-    %使用polyfit函数（一次多项式模型）对训练特征和量表得分进行拟合，求得系数
-    %在本示例代码中，分别使用正、负显著连边FC的和来进行拟合
-    fit_pos = polyfit(train_sumpos, train_behav,1);
-    fit_neg = polyfit(train_sumneg, train_behav,1);
 
-    %取出留一的测试样本，并求其正、负显著连边数量之和
-    test_mat = all_mats(:,:,leftout);
-    test_sumpos = sum(sum(test_mat.*pos_mask))/2;
-    test_sumneg = sum(sum(test_mat.*neg_mask))/2;
-    
-    %使用拟合出来的线性模型预测行为得分
-    behav_pred_pos(leftout) = fit_pos(1)*test_sumpos+fit_pos(2);
-    behav_pred_neg(leftout) = fit_neg(1)*test_sumneg+fit_neg(2);
-end
+    % For edges significantly present in all samples, save as a mask
+    consensus_feature = double(mean(consensus_feature, 3) == 1) + (-1) * double(mean(consensus_feature, 3) == -1);
 
-
-%在所有样本内都显著的连边，最终才保存为mask
-consensus_feature = double(mean(consensus_feature,3)==1) + (-1)*double(mean(consensus_feature,3)==-1);
-
-%评估模型预测的精度，同样使用Pearson Correlation来评估
-true_predict_r_pos = corr(behav_pred_pos, all_behav);
-true_predict_r_neg = corr(behav_pred_neg, all_behav);
-
-figure(1); plot(behav_pred_pos, all_behav,'r*');lsline;
-figure(2); plot(behav_pred_neg, all_behav,'b*'); lsline;
+    % Evaluate the predictive accuracy of the model, using Pearson Correlation
+    true_predict_r_pos = corr(behav_pred_pos, all_behav);
+    true_predict_r_neg = corr(behav_pred_neg, all_behav);
 end
